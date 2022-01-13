5663f70cb3f513de6b7894baeabee2b06921d93d
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-13 23:23:02 +0200

    Commit

--- HW_1/task_10.js
+++ HW_1/task_10.js
@@ -6,3 +6,15 @@
 // ```
 // **Внимание**!
 //     - Не разрашается использовать специальные методы массивов.
+
+var arr = [6,5,4,3,2,1];
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i ; j < arr.length; j++) {
+        if(arr[i] > arr[j]) {
+            var temp_element = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp_element
+        }
+    }
+    console.log(arr[i])
+}
