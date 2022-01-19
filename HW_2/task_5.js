0473fda86251b8c0d2bfedd70491ccce4afc6f25
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-19 21:23:11 +0200

    Commit

---
+++ HW_2/task_5.js
@@ -0,0 +1,34 @@
+// ####Задача 5
+// Дан массив с числами `[1, 2, -4, 3, -9, -1, 7]`.
+// Создайте из него новый массив, где останутся лежать только положительные числа `[1, 2, 3, 7]`.
+// Создайте для этого вспомогательную функцию `isPositive(-3)`, которая параметром будет принимать число и возвращать true, если число положительное,
+// и false — если отрицательное. Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число.
+// *Для добавление нового элемента в конец массива используйте метод push*
+// ```js
+// // Пример использования функции isPositvie()
+// isPositive(-3) // вернет false
+// isPositive(3) // вернет true
+// isPositive('s') // Error: parameter type is not a Number
+// // Пример использования метода push
+// var arr = [8];
+// arr.push(2);
+// console.log(arr[1]); // выведет число 2
+// ```
+
+// var result;
+var arr = [];
+function isPositive(){
+    for(item of arguments){
+        if (typeof item != 'number') {
+            throw new Error('Тип параметра не является числом / Parameter type is not a Numbe')
+        } else if (item > 0) {
+            result = true;
+            arr.push(item);
+        } else {
+            result = false;
+        }
+        // console.log(result);
+    }
+    console.log(arr);
+}
+isPositive(1, 2, -4, 3, -9, -1, 7);
