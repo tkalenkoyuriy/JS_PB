afe2f9ab9a34d35ef2273143eedefb8af14052e5
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-20 00:23:50 +0200

    Commit

--- HW_2/task_1.js
+++ HW_2/task_1.js
@@ -6,9 +6,26 @@
 // f(2); // 8
 // f('Content'); // Error: parameter type is not a Number
 // ```
+//-----------------------------------------------VAR_1-----------------------------------------------
+var result = 1
 function f(a){
     if(typeof a == 'number'){
-        a = a*a*a;
+        for (let i = 0; i < 3; i++) {
+            result = result * a;
+        }
+        console.log(result);
+    }else {
+        throw new Error('parameter type is not a Number')
+    }
+}
+f(2); // 8
+f('Content'); // Error: parameter type is not a Number
+
+//-----------------------------------------------VAR_2-----------------------------------------------
+
+function f(a){
+    if(typeof a == 'number'){
+        a = a * a * a;
         console.log(a);
     }else {
         throw new Error('parameter type is not a Number')
