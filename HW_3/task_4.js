cbe35d1684040f2ab25f34323fc028bca103f77c
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-27 19:01:10 +0200

    Commit

--- HW_3/task_4.js
+++ HW_3/task_4.js
@@ -7,22 +7,24 @@
 // const arr = [1,2,3];
 // some(arr, function(item, i, arr) {});
 // ```
-
+var newArr = [];
+var b;
 function some(arr, callback) {
     if  (!Array.isArray(arr)) throw new Error ('Параметр arr не является массивом');
     if  ( typeof callback !== 'function') throw new Error ('Параметр callback не является функцией');
     for (let i = 0; i < arr.length; i++) {
-
+        b = (arr[i] === 2) ? true : false
+        if (b === true){
+            callback(b)
+            break
+        }
     }
-
-
-
+    if(b === false) {
+        callback(b)
+    }
 }
-
 function callback (item,i){
-    console.log(item, i, arr);
+    console.log(item);
 }
-
-const arr = [1,2,3,4,-3,-2,6];
-
+const arr = [1,3,4,-3,-2,6];
 some(arr, callback);
