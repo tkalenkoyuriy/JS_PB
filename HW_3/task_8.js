c641867564ec19eeb0f5e9a919e4c67bb9c5b6fd
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-28 17:05:49 +0200

    Commit

--- HW_3/task_8.js
+++ HW_3/task_8.js
@@ -16,3 +16,27 @@
 // const arr5 = [[[[[],3]]]];
 // f(arr5); // 3
 // ```
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+const arr2 = [[[[[1,2]]]]];
+const arr3 = [[[[[1,2]]],2],1];
+const arr4 = [[[[[]]]]];
+const arr5 = [[[[[],3]]]];
+
+function f(arr) {
+    let summ = 0
+    if (!Array.isArray(arr)) throw new Error('Параметр arr не является массивом');
+    let arrNew = arr.flat(Infinity)
+    for (let i = 0; i < arrNew.length; i++) {
+        if (typeof arrNew[i] != "number"){
+            throw new Error('Элемент массива не являетася числом');
+        }else {
+            summ = summ + arrNew[i]
+        }
+    }
+    console.log(summ)
+}
+f(arr); // 12
+f(arr2); // 3
+f(arr3); // 6
+f(arr4); // 0
+f(arr5); // 3
