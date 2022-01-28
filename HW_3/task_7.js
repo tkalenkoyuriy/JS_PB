45464a8690cfa3cd54179f68859409e76c0b509f
tkalenkoyuriy <97669801+tkalenkoyuriy@users.noreply.github.com>
2022-01-28 14:54:24 +0200

    Commit

--- HW_3/task_7.js
+++ HW_3/task_7.js
@@ -12,10 +12,10 @@
 // Перепишите выделенный участок:
 // - используйте вместо цикла `for` метод `map`
 
-
 var arr = ['abcd', 'abcde', 'ab', 'abc'];
-var arrLength = [];
-for (var i = 0; i < arr.length; i++) {
-arrLength[i] = arr[i].length;
-}
+var arrLength = arr.map(function (n){
+    return n.length
+});
 console.log( arrLength ); // 4,5,2,3
+
+
